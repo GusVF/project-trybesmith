304d436a3b455f3b20647b3e5056b5d4b167cb79
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-05-08 17:53:06 -0300

    more fixes

--- src/middlewares/validateUser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var validateUser = function (err, _req, res, _next) {
-    if (err instanceof Error && err.message === 'UNAUTHORIZED') {
-        return res.status(401).json({ message: 'Username or password invalid' });
-    }
-    return res.status(500).json({ message: 'Internal error' });
-};
-exports.default = validateUser;
